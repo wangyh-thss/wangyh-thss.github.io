84600de5ebc121497fe2db129d93a94eda0d19fd
wangyh-thss <freedom6558@gmail.com>
2014-07-11 22:27:52 +0800

    work4-注释

--- js/comment.js
+++ js/comment.js
@@ -2,15 +2,18 @@
 	var commentRequest = new XMLHttpRequest();
 	var url;
 	var currentPage = 1;
-
+	//初始化评论区
 	initComment();
+	//本地存储，记录上一次浏览的页数
 	if(localStorage['commentPage']){
 		sendMessage(localStorage['commentPage']);
 	}else{
 		sendMessage(1);	
 	}
+	//向服务器请求数据 参数为页数
 	function sendMessage(page){
 		var pageNumber = parseInt(page);
+		//在第一页和最后一页分别禁用上一页和下一页
 		if(pageNumber == 1){
 			$('.previous').addClass('disabled');
 		}else{
@@ -24,7 +27,9 @@
 		$('#pageCount').text(pageNumber+' / 20 页')
 		url = '../data/comment/'+ pageNumber;
 		$('.form-control').val(pageNumber);
+		//本地储存浏览数据
 		localStorage['commentPage'] = page;
+		//通信
 		commentRequest.onreadystatechange = handler;
 		commentRequest.open('GET', url);
 		commentRequest.send();
@@ -41,7 +46,7 @@
 			}
 		}
 	}
-
+	//向评论区添加所需的容器
 	function initComment(){
 		var comments = $('#comments');
 		comments.append('<div id="commentTitle" style="border-bottom:1px solid #393939;"></div>');
@@ -50,25 +55,25 @@
 		setTitle();
 		setControl();
 	}
-
+	//标题容器
 	function setTitle(){
 		var titleBar = $('#commentTitle');
 		titleBar.append('<p style="font-size:25px; color:#393939; font-family:Microsoft YaHei; margin-bottom:0px;">评论<span style="font-size:15px">（伪）</span></p>')
 		titleBar.append('<p id="pageCount"></p>')
 	}
-
+	//控件容器
 	function setControl(){
+		//添加控件：上一页  选择页码  下一页
 		var controlBar = $('#commentControl');
 		controlBar.append('<a class="btn btn-default next" href="#commentTitle" role="button">下一页</a>');
-		
 		var select = $('<select class="short form-control" style="float:right"></select>');
 		for(var i = 1; i <= 20; i++){
 			select.append('<option value="'+i+'">'+i+'</option>');
 		}
 		select.wrap('<span></span>')
 		controlBar.append(select);
-		
 		controlBar.append('<a class="btn btn-default previous" href="#commentTitle" role="button">上一页</a>');
+		//绑定控件触发的事件
 		$('.previous').click(function(){
 			if(currentPage > 1){
 				currentPage = currentPage - 1;
@@ -87,32 +92,38 @@
 			sendMessage(currentPage);
 		})
 	}
-
+	//载入评论数据
 	var list;
 	function loadComment(data){
-		if(list)
-			list.remove();
+		//移除现有数据
+		if(list) list.remove();
+		//添加目标数据
 		var commentContainer = $('#commentContainer');
 		commentContainer.append('<ul id="commentList"></ul>');
 		list = $('#commentList');
+		//逐条添加评论
 		for(var i = 0; data.data.commentid[i]; i++){
+			//某一项评论的整体
 			var item = $('<li class="commentItem"></li>');
+			//用户信息
 			var user = $('<div class="userInfo"></div>');
 			user.append('<img src="'+data.data.commentid[i].userinfo.head+'" class="userHead" />');
 			user.append('<p class="userID">'+data.data.commentid[i].userinfo.nick+'</p>');
-
+			//评论内容
 			var content = $('<div style="margin-top:14px;"></div>')
 			content.append('<div class="commentContent"><p class="commentText">'+data.data.commentid[i].content+'</p></div>');
+			//时间信息和赞
 			var info = $('<div class="commentInfo"></div>');
 			info.append('<span class="timeInfo">'+data.data.commentid[i].timeDifference+'</span>');
 			info.append('<span class="zan">('+data.data.commentid[i].up+')<span>');
 			content.append(info);
-
+			//将上述模块添加到列表项中
 			item.append(user);
 			item.append(content);
 			item.append('<div id="clearfix"></div>')
 			list.append(item);
 		}
+		//绑定“赞”的点击事件 仅仅实现了视觉效果，没有与服务器的数据传输
 		$('.zan').click(function(){
 			var string = $(this).text();
 			var num = parseInt(string.split('(')[1].split(')')[0]);
--- js/picture.js
+++ js/picture.js
@@ -1,6 +1,6 @@
 ﻿void function picture(){
 	var url = '../data/picture.json';
-
+	//与服务器的通信
 	var pictureSlide = new XMLHttpRequest();
 	pictureSlide.onreadystatechange = handler;
 	pictureSlide.open('GET', url);
@@ -17,28 +17,26 @@
 			}
 		}
 	}
-
+	//主要功能函数
 	function initPlayer(data){
 		newsTitleArray = [];
 		var container = $('#picplayer');
 		var w = container.css('width').split('px')[0];
 		var h = eval(w) / 1000 * 550;
+		//添加所需容器
 		container.css('height', h+'px');
 		container.append('<div id="picContent" style="padding:0px 0px; margin:0px 0px; overflow:hidden;"></div>');
-		//container.append('<div id="titleBackground" class= "translucent" style="width:'+w+'px; height:70px; background-color: #808080; text-align:center; z-index:100; position:absolute;"></div>');
-		//container.append('<div id="titleContainer" style="width:'+w+'px; padding-top: 12px; height:70px; text-align:center; z-index:101; position:absolute;"></div>');
-		$('#titleContainer').append('<a id="newsTitle" href="#" style="text-decoration:none; font-size:36px; color:#fff; font-family:Microsoft YaHei;" target="_blank"></a>')
 		var pic = $('#picContent');
-
-		container.append('<div id="numContainer" style="padding:0px 0px; margin-top:'+(h-40)+'px;height:40px; width:'+w+'px; z-index:99; position:absolute;"></div>')
+		container.append('<div id="numContainer" style="padding:0px 0px; margin-top:'+(h-40)+'px;height:40px; width:70%; z-index:99; position:absolute;"></div>')
 		var num = $('#numContainer');
-		num.append('<div id="numList" style="margin:10px 20px; height:20px; float:right;"></div>')
+		num.append('<div id="numList" style="z-index:0; margin:10px 20px; height:20px; float:right;"></div>')	
 		num = $('#numList');
 
 		for(var i = 0; data.picture[i]; i++){
+			//将图片依次插入，置于不同层，且每张图添加相应的新闻信息
 			pic.append('\
-				<div id= "pic'+i+'" class="picView" style="width:'+w+'px; display: none; z-index:'+i+'; position:absolute;">\
-					<img src="'+data.picture[i].url+'" style="width:'+w+'px;">\
+				<div id= "pic'+i+'" class="picView" style="width:70%; display: none; z-index:'+i+'; position:absolute; overflow:hidden;">\
+					<img src="'+data.picture[i].url+'" style="width:100%;">\
 					<div class="mask">\
 						<h2>'+ data.picture[i].title +'</h2>\
 						<p>'+data.picture[i].info+'</p>\
@@ -46,31 +44,30 @@
 					</div>\
 				</div>\
 				');
+			//插入序号控件
 			num.append('<span id="num'+i+'" class="translucent" style="margin:0px 6px; background-color:#393939; color:#e6e6e6; padding: 2px 4px; border:1px solid #e6e6e6;font-size:10px; cursor:pointer;">'+(i+1)+'</span>');
-			newsTitleArray[i] = data.picture[i].title;
 		}
-
+		//绑定鼠标指向序号控件事件（闭包）
 		function mouseoverEvent(number){
 			return function(){
 				showPicture(number);
 			}
 		}
-
 		for(var i = 0; data.picture[i]; i++){
 			$('#num'+i)[0].onmousemove = mouseoverEvent(i);
-			//$('#num'+i)[0].onmouseout = mouseoutEvent(i);
 		}
-
+		//HTML5本地存储，若有数据则从上一次查看的图片开始播放
+		
+		//图片轮播的实现
+		var timer;
+		var selectedIndex;
+		var currentNum;
+		var currentPicture;
 		if(localStorage['pictureNum']){
 			showPicture(localStorage['pictureNum']);
 		}else{
 			showPicture(0);	
 		}
-		
-		var timer;
-		var selectedIndex;
-		var currentNum;
-		var currentPicture;
 		function showPicture(index){
 			clearInterval(timer);
 			if(selectedIndex != index){
@@ -87,16 +84,15 @@
 				currentPicture = $('#pic'+index);
 				currentPicture.fadeIn('slow');
 				localStorage['pictureNum']  = selectedIndex;
-				$('#newsTitle').text(newsTitleArray[index]);
-				$('#newsTitle').attr('href', currentPicture.parent().attr('href'));
 			}
+			//循环播放
 			timer = setInterval(function(){
-				var nextIndex = selectedIndex+1;
+				var nextIndex = eval(selectedIndex)+1;
 				if(nextIndex < data.picture.length)
 					showPicture(nextIndex);
 				else
 					showPicture(0);
-			}, 5000);
+			}, 4000);
 		}
 	}
 }();
